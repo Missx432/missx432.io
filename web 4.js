cd377c9406d7c33687574eebc1dde0ae2949bcf3
Missx432 <hary.fahry09@gmail.com>
2025-01-26 08:26:56 +0700

    missx432.io

---
+++ web 4.js
@@ -0,0 +1,8 @@
+function redirectToWebsite() {
+    window.location.href = "https://saweria.co/missx"; // Ganti dengan URL tujuan
+}
+
+function openApp() {
+    window.location.href = "https://api.whatsapp.com/send/?phone=62881023573523&text&type=phone_number&app_absent=0&wame_ctl=1"; // Ganti dengan schema aplikasi tujuan
+}
+
